9bd8120fa6a2fac8ba0f69e82af7426640e410c5
Emily Landgrebe <emilylandgrebe@Emilys-MacBook-Pro.local>
2019-08-19 10:54:36 -0500

    help

--- controllers/destinations.js
+++ controllers/destinations.js
@@ -8,9 +8,6 @@ function create(req, res) {
   Flight.findById(req.params.id, (err, flight) => {
     flight.destinations.push(req.body);
     flight.save(function(err) {
-    for(let key in req.body) {
-      if(req.body[key] === '') delete req.body[key];
-    }
       res.redirect(`/flights/${flight._id}`);
     });
   });
